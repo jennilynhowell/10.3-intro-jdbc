e0b865206be4e3ee1fa100854149a40c9f2462b1
Jennilyn Howell <jennilynhowell@me.com>
2017-07-26 09:46:20 -0400

    Able to create & drop table

---
+++ src/com/jennilyn/Main.java
@@ -0,0 +1,18 @@
+package com.jennilyn;
+
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+	    Class.forName("org.sqlite.JDBC");
+
+	    try (Connection connection = DriverManager.getConnection("jdbc:sqlite:stats.db")) {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("DROP TABLE IF EXISTS stats");
+            statement.executeUpdate("CREATE TABLE stats (id INTEGER PRIMARY KEY, string STRING, wins INTEGER, losses INTEGER)");
+        } catch (SQLException ex) {
+            System.out.println("Something went wrong with your database connections.");
+        }
+    }
+}
