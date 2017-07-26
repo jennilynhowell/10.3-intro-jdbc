25ff289ea3c6c196eed6f9db69ff125cf1b70c94
Jennilyn Howell <jennilynhowell@me.com>
2017-07-26 11:55:49 -0400

    Starting to build UI

--- src/com/jennilyn/Main.java
+++ src/com/jennilyn/Main.java
@@ -6,6 +6,7 @@ import com.jennilyn.Model.Stat;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
@@ -13,34 +14,43 @@ public class Main {
 	    Class.forName("org.sqlite.JDBC");
 
 	    try (Connection connection = DriverManager.getConnection("jdbc:sqlite:stats.db")) {
-            DatabaseManager db = new DatabaseManager(connection);
-            Statement statement = db.getStatement();
 
-            db.dropStatsTable();
-            db.createStatsTable();
+            //let's make a UI for this app
 
-            Stat lukeBaseballStats = new Stat("Luke", 24, 0, statement);
-            lukeBaseballStats.saveStat();
-
-            Stat joeMontana = new Stat("Joe Montana", 750, 2, statement);
-            joeMontana.saveStat();
-
-            List<Stat> results = Stat.findAll(db);
-            for (Stat stat : results) {
-                //ok, let's update all wins to 0, then update them
-                //System.out.println(stat);
-                stat.setWins(0);
-                stat.updateStat();
-            }
-
-            results = Stat.findAll(db);
-            for (Stat stat : results) {
-                System.out.println(stat);
-            }
+            welcomeMenu();
 
         } catch (SQLException ex) {
             System.out.println("Something went wrong with your database connections.");
             ex.printStackTrace();
         }
+    }
+
+    public static void welcomeMenu() {
+        System.out.println("\n================");
+        System.out.println("Welcome to Stat Database 5400! What would you like to do?");
+        System.out.println("1) Show all stats.");
+        System.out.println("2) Add a new stat.");
+        System.out.println("3) Update an existing stat.");
+        System.out.println("================");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        switch (choice){
+            case 1:
+                System.out.println("Now showing all stat data.");
+                break;
+            case 2:
+                System.out.println("What's your new stat?");
+                break;
+            case 3:
+                System.out.println("Which stat would you like to update?");
+                break;
+            default:
+                System.out.println("Sorry, that's not an option on this menu.");
+                break;
+        }
+
+        welcomeMenu();
     }
 }
